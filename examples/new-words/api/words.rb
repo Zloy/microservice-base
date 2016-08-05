ec23c3e361294be2f61383407960518a85815db4
Zloy <zhoran@inbox.ru>
2016-08-05 13:09:47 +0000

    Refactor app.rb

---
+++ examples/new-words/api/words.rb
@@ -0,0 +1,29 @@
+get '/w', auth: :user do
+  content_type 'application/json'
+
+  Word.all(session[:user_id]).to_json
+end
+
+post '/w/:word', auth: :user do
+  request.body.rewind
+  payload = request.body.read
+
+  content_type 'application/json'
+  status 201
+
+  Word.insert_or_update(session[:user_id], params[:word], payload).to_json
+end
+
+put '/w/:word/learned', auth: :user do
+  status 204
+  body nil
+
+  Word.learned(session[:user_id], params[:word])
+end
+
+delete '/w/:word', auth: :user do
+  status 204
+  body nil
+
+  Word.delete(session[:user_id], params[:word])
+end
