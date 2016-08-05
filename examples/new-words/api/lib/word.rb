ba2b8c381658d548d352173172fa8ac20761b434
Zloy <zhoran@inbox.ru>
2016-08-05 13:31:59 +0000

    Add validation to words controller

--- examples/new-words/api/lib/word.rb
+++ examples/new-words/api/lib/word.rb
@@ -1,4 +1,6 @@
 module Word
+  WORD_VALIDATION_REGEXP = /\A[a-z]+\Z/i
+
   def self.all(user_id)
     if user_id == 1234
       { some: :data }
@@ -22,5 +24,9 @@ module Word
 
   def self.send(str)
     @send_lambda.call(str)
+  end
+
+  def self.valid?(word)
+    WORD_VALIDATION_REGEXP =~ word
   end
 end
--- examples/new-words/api/words_controller.rb
+++ examples/new-words/api/words_controller.rb
@@ -1,10 +1,22 @@
+register do
+  def validate(type)
+    condition { halt 422 unless send("#{type}_valid?") }
+  end
+end
+
+helpers do
+  def word_valid?
+    Word.valid?(params[:word])
+  end
+end
+
 get '/w', auth: :user do
   content_type 'application/json'
 
   Word.all(session[:user_id]).to_json
 end
 
-post '/w/:word', auth: :user do
+post '/w/:word', auth: :user, validate: :word do
   request.body.rewind
   payload = request.body.read
 
@@ -14,14 +26,14 @@ post '/w/:word', auth: :user do
   Word.insert_or_update(session[:user_id], params[:word], payload).to_json
 end
 
-put '/w/:word/learned', auth: :user do
+put '/w/:word/learned', auth: :user, validate: :word do
   status 204
   body nil
 
   Word.learned(session[:user_id], params[:word])
 end
 
-delete '/w/:word', auth: :user do
+delete '/w/:word', auth: :user, validate: :word do
   status 204
   body nil
 
