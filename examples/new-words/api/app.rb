18cd3e3bb1bd7d4e113c2a65b2dbb14d658bb47e
Zloy <zhoran@inbox.ru>
2016-08-05 13:10:24 +0000

    Refactor app.rb

--- examples/new-words/api/app.rb
+++ examples/new-words/api/app.rb
@@ -3,14 +3,13 @@ require 'sinatra-initializers'
 require File.expand_path 'load_paths.rb', File.dirname(__FILE__)
 
 class App < Sinatra::Application
+  register Sinatra::Initializers
+
   require 'setup_logger'
   require 'login'
-
-  register Sinatra::Initializers
+  require 'words'
 
   get '/status' do
     'ok'
   end
-
-  require 'words'
 end
