9768da6f58cb5eaeed44a343bf711252cfecddfb
Zloy <zhoran@inbox.ru>
2016-08-10 21:42:32 +0300

    Don't initialize connection to RabbitMQ in test env

--- examples/new-words/api/config/initializers/02_word_job_initializer.rb
+++ examples/new-words/api/config/initializers/02_word_job_initializer.rb
@@ -2,15 +2,19 @@ require 'bunny_pool'
 require 'word_job'
 require 'json'
 
-bunny_pool = BunnyPool.new.configure do |config|
-  # defaults:
-  # config.host = :localhost
-  # config.port = 5672
-  # config.pool_size = 5
-  # config.pool_timeout = 5 # seconds
-  config.queue = 'words jobs'
-end
-
-WordJob.send_lambda = bunny_pool.send_lambda
-
 WordJob.serialize_lambda = -> (obj) { JSON.generate(obj) }
+
+if ENV['RACK_ENV'] == 'test'
+  WordJob.send_lambda = -> { raise 'Unexpectedly Word.send() called' }
+else
+  bunny_pool = BunnyPool.new.configure do |config|
+    # defaults:
+    # config.host = :localhost
+    # config.port = 5672
+    # config.pool_size = 5
+    # config.pool_timeout = 5 # seconds
+    config.queue = 'words jobs'
+  end
+
+  WordJob.send_lambda = bunny_pool.send_lambda
+end
